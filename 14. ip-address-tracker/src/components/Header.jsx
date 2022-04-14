64a884f1cca05e9cf5e5fe98041d0643b3eb656b
Matt Davis <65673857+mattdavis06@users.noreply.github.com>
2022-04-14 16:54:30 +0100

    Add files via upload

---
+++ 14. ip-address-tracker/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import SearchInput from './SearchInput'
+
+function Header() {
+  return (
+    <header>
+      <h1>IP Address Tracker</h1>
+      <SearchInput />
+    </header>
+  )
+}
+
+export default Header
