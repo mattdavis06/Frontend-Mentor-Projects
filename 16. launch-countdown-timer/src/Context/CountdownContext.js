b66716ab006ab46d395bb99c42be9523e6a83545
Matt Davis <65673857+mattdavis06@users.noreply.github.com>
2022-07-28 14:01:18 +0100

    Add files via upload

--- 16. launch-countdown-timer/src/Context/CountdownContext.js
+++ 16. launch-countdown-timer/src/Context/CountdownContext.js
@@ -3,54 +3,54 @@ import { createContext, useState, useEffect } from 'react'
 const CountdownContext = createContext()
 
 export const CountdownProvider = ({ children }) => {
-  const calculateTimeLeft = () => {
-    let year = new Date().getFullYear()
-    let difference = +new Date(`06/30/${year}`) - +new Date()
+	const calculateTimeLeft = () => {
+		let year = new Date().getFullYear() + 1
+		let difference = +new Date(`06/30/${year}`) - +new Date()
 
-    let timeLeft = {}
+		let timeLeft = {}
 
-    if (difference > 0) {
-      timeLeft = [
-        {
-          id: 1,
-          countdownText: 'days',
-          countdownValue: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        },
-        {
-          id: 2,
-          countdownText: 'hours',
-          countdownValue: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        },
-        {
-          id: 3,
-          countdownText: 'minutes',
-          countdownValue: Math.floor((difference / 1000 / 60) % 60),
-        },
-        {
-          id: 4,
-          countdownText: 'seconds',
-          countdownValue: Math.floor((difference / 1000) % 60),
-        },
-      ]
-    }
-    return timeLeft
-  }
+		if (difference > 0) {
+			timeLeft = [
+				{
+					id: 1,
+					countdownText: 'days',
+					countdownValue: Math.floor(difference / (1000 * 60 * 60 * 24)),
+				},
+				{
+					id: 2,
+					countdownText: 'hours',
+					countdownValue: Math.floor((difference / (1000 * 60 * 60)) % 24),
+				},
+				{
+					id: 3,
+					countdownText: 'minutes',
+					countdownValue: Math.floor((difference / 1000 / 60) % 60),
+				},
+				{
+					id: 4,
+					countdownText: 'seconds',
+					countdownValue: Math.floor((difference / 1000) % 60),
+				},
+			]
+		}
+		return timeLeft
+	}
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft())
-    }, 1000)
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setTimeLeft(calculateTimeLeft())
+		}, 1000)
 
-    return () => clearTimeout(timer)
-  })
+		return () => clearTimeout(timer)
+	})
 
-  return (
-    <CountdownContext.Provider value={{ timeLeft }}>
-      {children}
-    </CountdownContext.Provider>
-  )
+	return (
+		<CountdownContext.Provider value={{ timeLeft }}>
+			{children}
+		</CountdownContext.Provider>
+	)
 }
 
 export default CountdownContext
